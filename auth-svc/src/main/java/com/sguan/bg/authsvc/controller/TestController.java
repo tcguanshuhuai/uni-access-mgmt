eed86ba5ee43dbefdc82774a11f8138d4f3154bb
guanshuhuai <guanshuuai@outlook.com>
2024-07-03 20:13:09 -0700

    init

--- auth-svc/src/main/java/com/sguan/bg/authsvc/controller/TestController.java
+++ auth-svc/src/main/java/com/sguan/bg/authsvc/controller/TestController.java
@@ -1,12 +1,9 @@
 package com.sguan.bg.authsvc.controller;
 
-import com.sguan.bg.authsvc.annotations.ResultWrapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@ResultWrapper
 @RequestMapping("/test")
 @RestController
 public class TestController {
